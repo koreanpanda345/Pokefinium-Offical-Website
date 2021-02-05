c69254ed9481e92c155c8cbf62149c29f55ae178
Cody Spratford <36426888+koreanpanda345@users.noreply.github.com>
2021-02-04 16:53:34 -0800

    Adding a search feature for the draft page

--- pages/drafts.vue
+++ pages/drafts.vue
@@ -1,54 +1,79 @@
 <template>
   <div>
-    <div v-if="drafts.length == 0">
+
+    <!-- <div v-if="drafts.length == 0">
       {{getDraftsForSeason7}}
+    </div> -->
+
+    <div>
+      <v-data-table
+      :headers="headers"
+      :items="drafts"
+      item-key="name"
+      class="elevation-1"
+      :search="search"
+      :custom-filter="filterDrafts">
+      <template v-slot:top>
+        <v-text-field
+        v-model="search"
+        label="Search For Draft"
+        class="mx-4"></v-text-field>
+      </template>
+      <template v-slot:body.append>
+        <tr>
+          <td></td>
+        </tr>
+
+      </template>
+      </v-data-table>
     </div>
-    <v-row justify="center" align="center">
-      <v-col cols="12" sm="8" md="6" v-for="draft in drafts" :key="draft">
-        <v-card>
-          <v-card-title>
-            Season {{draft.season.split("season_")[1]}} {{draft.name}}
-          </v-card-title>
-          <v-card-subtitle >
-          <v-row justify="center" align="center">
-            <h4>Liaisons</h4>
-            <v-spacer/>
-            <div v-for="liaison in draft.liaisons" :key="liaison">
-              <v-chip :color="liaison.color">{{liaison.name}}</v-chip>
-            </div>
-          </v-row>
-          <br>
-          <br>
-          <v-row justify="center" align="center">
-          <h4>Status</h4>
-          <v-spacer/>
-          <v-chip v-if="draft.isCurrent" color="green">
-            Ongoing
-          </v-chip>
-          <v-chip v-else color="brown lighten-3">
-            Old
-          </v-chip>
-          </v-row>
-          </v-card-subtitle>
-          <v-btn :href="draft.draft_doc">View Doc</v-btn>
-        </v-card>
-      </v-col>
-    </v-row>
   </div>
 </template>
 <script>
 export default {
   data: () => ({
+    search: "",
     drafts: []
   }),
   computed: {
+    headers() {
+      return [
+        {
+          text: 'League',
+          align: 'start',
+          sortable: false,
+          value: 'name'
+        },
+        {
+          text: 'Season',
+          value: 'season',
+        },
+        {
+          text: 'Liaisons',
+          value: 'liaisons'
+        },
+        {
+          text: 'Doc',
+          value: 'draftDoc'
+        }
+      ]
+    },
     async getDraftsForSeason7() {
-      await this.$axios.get("https://pokemon-backend.netlify.app/.netlify/functions/api/draft/season_7/")
+      await this.$axios.get("https://pokemon-backend.netlify.app/.netlify/functions/api/draft/")
         .then(({data}) =>
         {
           this.drafts = data;
           console.log(data);
         }).catch(error => console.error(error));
+    }
+  },
+  methods: {
+    filterDrafts(value, search, item)
+    {
+      return value != null &&
+       search != null &&
+        typeof value === 'string' &&
+         value.toString().toLocaleUpperCase().indexOf(search.toUpperCase()) !== -1
     }
   }
 }
