3babebce4c696fe0d561467b53b2748ffc54784b
Cody Spratford <36426888+koreanpanda345@users.noreply.github.com>
2021-02-04 17:13:33 -0800

    Scraping that idea

--- pages/drafts.vue
+++ pages/drafts.vue
@@ -1,81 +1,54 @@
 <template>
   <div>
-
-    <!-- <div v-if="drafts.length == 0">
+    <div v-if="drafts.length == 0">
       {{getDraftsForSeason7}}
-    </div> -->
-    <div v-if="drafts.length === 0">
-      {{getDrafts}}
     </div>
-    <div>
-      <v-data-table
-      :headers="headers"
-      :items="drafts"
-      item-key="name"
-      class="elevation-1"
-      :search="search"
-      :custom-filter="filterDrafts">
-      <template v-slot:top>
-        <v-text-field
-        v-model="search"
-        label="Search For Draft"
-        class="mx-4"></v-text-field>
-      </template>
-      <template v-slot:body="{drafts}">
-          <tbody>
-        <tr v-for="draft in drafts" :key="draft">
- 
-          <td>{{draft.name}}</td>
-          <td>{{draft.season}}</td>
-          <td><a :href="draft.draftDoc">View Doc</a></td>
-        </tr>
-          </tbody>
-      </template>
-      </v-data-table>
-    </div>
+    <v-row justify="center" align="center">
+      <v-col cols="12" sm="8" md="6" v-for="draft in drafts" :key="draft">
+        <v-card>
+          <v-card-title>
+            Season {{draft.season.split("season_")[1]}} {{draft.name}}
+          </v-card-title>
+          <v-card-subtitle >
+          <v-row justify="center" align="center">
+            <h4>Liaisons</h4>
+            <v-spacer/>
+            <div v-for="liaison in draft.liaisons" :key="liaison">
+              <v-chip :color="liaison.color">{{liaison.name}}</v-chip>
+            </div>
+          </v-row>
+          <br>
+          <br>
+          <v-row justify="center" align="center">
+          <h4>Status</h4>
+          <v-spacer/>
+          <v-chip v-if="draft.isCurrent" color="green">
+            Ongoing
+          </v-chip>
+          <v-chip v-else color="brown lighten-3">
+            Old
+          </v-chip>
+          </v-row>
+          </v-card-subtitle>
+          <v-btn :href="draft.draft_doc">View Doc</v-btn>
+        </v-card>
+      </v-col>
+    </v-row>
   </div>
 </template>
 <script>
 export default {
   data: () => ({
-    search: "",
     drafts: []
   }),
   computed: {
-    headers() {
-      return [
-        {
-          text: 'League',
-          align: 'start',
-          sortable: false,
-          value: 'name'
-        },
-        {
-          text: 'Season',
-          value: 'season',
-        },
-        {
-          text: 'Doc',
-          value: 'draftDoc'
-        }
-      ]
-    },
-    async getDrafts() {
+    async getDraftsForSeason7() {
       await this.$axios.get("https://pokemon-backend.netlify.app/.netlify/functions/api/draft/")
         .then(({data}) =>
         {
           this.drafts = data;
           console.log(data);
         }).catch(error => console.error(error));
-    }
-  },
-  methods: {
-    filterDrafts(value, search, item)
-    {
-      return value != null &&
-       search != null &&
-        typeof value === 'string' &&
-         value.toString().toLocaleUpperCase().indexOf(search.toUpperCase()) !== -1
     }
   }
 }
